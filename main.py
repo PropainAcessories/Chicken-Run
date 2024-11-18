52958bddd856a6c7c900175755594a56bb359a8a
Akbar Zob <fieldmarshalbonespur69@gmail.com>
2024-11-18 12:25:59 -0600

    basic barebones boilerplate, will convert images to pixels either tonight or tomorrow and render complete background into pygame window.

--- main.py
+++ main.py
@@ -1,6 +1,29 @@
 import pygame
+from pygame.locals import *
+
+SCREEN_WIDTH = 1280
+SCREEN_HEIGHT = 720
+GROUND_HEIGHT = SCREEN_HEIGHT - 70
+PLAY_GAME = True
 
 pygame.init()
-screen = pygame.display.set_mode((1280, 720))
+window = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption('Chicken Run')
+
 clock = pygame.time.Clock()
-running = True
+FPS = 30
+#Possible audio later
+
+def Start_Game():
+  run = True
+  play_again = False
+  game_over = False
+  # Game speed later
+  
+  while run:
+    clock.tick(FPS)
+    
+    
+  # Will keep the main loop running as long as the player wants to play it.  
+  while PLAY_GAME:
+    PLAY_GAME = Start_Game()
